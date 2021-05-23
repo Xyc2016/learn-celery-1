384f768a079eb6526225dee45979e9a55cbe215f
xyc <986327386@qq.com>
2021-05-23 17:24:47 +0800

    MOD

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
 import pymongo
-db_client = pymongo.MongoClient("localhost", 27017)
+db_client = pymongo.MongoClient("localhost", 27017, connect=False)
 learn_celery_db = db_client["learn_celery_db"]
 learn_celery_collection = learn_celery_db["learn_celery_collection"]
